52cdc7e9935055c843a7516b5af73bf39ae2fa3a
willhall88 <willhall88@hotmail.com>
2014-08-08 17:43:31 +0100

    added a generic fail message for bad inputs

--- ruby-code-test/start_editing.rb
+++ ruby-code-test/start_editing.rb
@@ -8,8 +8,12 @@ instance = Commands.new
 input = ""
 
 loop do
+	begin
 	puts "enter command:"
 	instance.process_input(STDIN.gets.chomp)
+	rescue 
+		puts 'Oops, something went wrong. Please try again.'
+	end
 end
 
 
