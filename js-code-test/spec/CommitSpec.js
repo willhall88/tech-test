e78925b5ce758e52881a22a5a807dcc84e1c9b49
willhall88 <willhall88@hotmail.com>
2014-08-10 17:57:49 +0100

    now have an array of users with their commit count and avatars ready to be resized.

--- js-code-test/spec/CommitSpec.js
+++ js-code-test/spec/CommitSpec.js
@@ -1,9 +1,33 @@
 describe("Github Commits", function() {
-  var player;
-  var song;
+  var committer1 = {id: 1, avatar_url: "red-circle.jpg", other_info: "not needed"}
+  var committer2 = {id: 2, avatar_url: "red-triangle.jpg", other_info: "not needed"}
+
+  var commits = [
+    {committer: committer1}, 
+    {committer: committer2},
+    {committer: committer1}
+  ];
+
+  var imageSizer;
 
   beforeEach(function() {
-    player = new Player();
-    song = new Song();
+    imageSizer = new ImageSizer(commits);
   });
+
+  it("create a new array with relevant data", function() {
+    var committerData1 = {id: 1, avatar_url: "red-circle.jpg"}
+    var committerData2 = {id: 2, avatar_url: "red-triangle.jpg"}
+    var commitData = [committerData1, committerData2, committerData1]
+    expect(imageSizer.pullData()).toEqual (commitData);
+  });
+
+  it("adds the commit count to each committer", function(){
+    var committerData1 = {id: 1, avatar_url: "red-circle.jpg", commits: 2}
+    var committerData2 = {id: 2, avatar_url: "red-triangle.jpg", commits: 1}
+    var commitData = [committerData1, committerData2]
+    expect(imageSizer.checkCommits()).toEqual (commitData);
+  })
+
 });
+
+
--- js-code-test/src/Commits.js
+++ js-code-test/src/Commits.js
@@ -1,3 +1,58 @@
-function Commits() {
+function ImageSizer(commits) {
+	this.commitArray = commits
 }
 
+
+ImageSizer.prototype.pullData = function() { 
+	newArray =[];
+	this.commitArray.forEach(function(commit){
+		newArray.push({id: commit.committer.id, avatar_url: commit.committer.avatar_url});
+	});
+	return newArray;
+};
+
+ImageSizer.prototype.checkCommits = function() {
+	newArray = this.pullData();
+	newArray.sort(sortById);
+	this.addCommits(newArray);
+	this.removeDuplicates(newArray);
+	return newArray;
+};
+
+
+ImageSizer.prototype.removeDuplicates = function(array){
+	var previousCommit = 0
+	array.reverse().forEach(function(commit){
+		if (commit.id == previousCommit.id){
+			i = array.indexOf(commit);
+			array.splice(i, 1);
+		}
+		previousCommit = commit;
+	});
+	array.reverse();
+}
+
+ImageSizer.prototype.addCommits = function(array){
+	var previousCommit = 0;
+	var count = 1;
+	newArray.forEach(function(commit){
+		if (commit.id === previousCommit.id){
+			count++;
+			commit.commits = count;
+		} else {
+			commit.commits = 1;
+			count = 1
+		}
+		previousCommit = commit;
+	});
+}
+
+function sortById(first, second){
+	if (first.id > second.id){
+		return 1;
+	} 
+	if (first.id < second.id) {
+		return -1;
+	}
+	return 0;
+}
