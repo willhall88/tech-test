7369f296226ea47261f1d1b4688a18a53bb70fdd
willhall88 <willhall88@hotmail.com>
2014-08-13 11:01:11 +0100

    made some style changes

--- js-code-test/src/Commits.js
+++ js-code-test/src/Commits.js
@@ -1,76 +1,62 @@
-function ImageSizer(commits) {
-	this.commitArray = commits;
+function ImageSizer(commitCount) {
+  this.commits = commitCount;
 };
 
 ImageSizer.prototype.calculateSize = function(){
-	var users = this.findCommitsByUser();
-	
-	users.forEach(function(user){
-		user.avatar_size = user.commits * 10 + 40;
-	});
-	return users;
+  var users = this.findCommitsByUser();
+  
+  users.forEach(function(user){
+    user.avatarSize = user.commits * 10 + 40;
+  });
+  return users;
 };
 
 ImageSizer.prototype.findCommitsByUser = function() {
-	var pulledData = this.pullData();
-
-	pulledData.sort(sortById);
-	this.addCommits(pulledData);
-	
-	var uniqueData = this.removeDuplicates(pulledData);
-	return uniqueData;
+  var pulledData = this.pullData();  
+  var uniqueData = this.addCommits(pulledData);
+  
+  return uniqueData;
 };
 
 ImageSizer.prototype.pullData = function() { 
-	var pulledData =[];
-	
-	this.commitArray.forEach(function(commit){
-		pulledData.push({id: commit.committer.id, avatar_url: commit.committer.avatar_url});
-	});
-	return pulledData;
+  var pulledData = this.commits.map(function(commit){
+    return {id: commit.committer.id, 
+      avatarUrl: commit.committer.avatar_url
+    };
+  });
+  return pulledData;
 };
 
 ImageSizer.prototype.addCommits = function(array){
-	var previousCommit = 0;
-	var count = 1;
+  array.sort(this.sortById);
 
-	array.forEach(function(commit){
-		if (commit.id === previousCommit.id){
-			count++;
-			commit.commits = count;
-		} else {
-			commit.commits = 1;
-			count = 1;
-		}
-		previousCommit = commit;
-	});
-}
-
-ImageSizer.prototype.removeDuplicates = function(array){
-	var uniqArray = [];
-	
-	while (array.length > 1) {
-		if (array[0].id === array[1].id){
-			array.shift();
-		} else {
-			uniqArray.push(array[0]);
-			array.shift();
-		}
-	}
-	if (array[0].id !== uniqArray.reverse()[0].id){
-		uniqArray.push(array[0]);
-	}
-
-	return uniqArray;
+  var uniqArray = [];
+  var previousCommit = 0;
+  var count = 1;  
+  array.forEach(function(commit){
+    if (commit.id === previousCommit.id){
+      count++;
+      commit.commits = count;
+    } else {
+      uniqArray.push(previousCommit)
+      commit.commits = 1;
+      count = 1;
+    }
+    previousCommit = commit;
+  });
+  uniqArray.push(previousCommit);
+  uniqArray.splice(0,1);  
+  return uniqArray;
 }
 
 
-function sortById(first, second){
-	if (first.id > second.id){
-		return 1;
-	} 
-	if (first.id < second.id) {
-		return -1;
-	}
-	return 0;
+
+ImageSizer.prototype.sortById = function(first, second){
+  if (first.id > second.id){
+    return 1;
+  } 
+  if (first.id < second.id) {
+    return -1;
+  }
+  return 0;
 }
--- js-code-test/src/application.js
+++ js-code-test/src/application.js
@@ -1,20 +1,37 @@
-
 $(document).ready(function() {
-  $.get('https://api.github.com/repos/rails/rails/commits', function(commits){
-		firstTwentyCommits = commits.slice(0,20);
-		var imageSizer = new ImageSizer(firstTwentyCommits);
-
-		var users = imageSizer.calculateSize();
-		addAvatar(users);
-		
-	});
-
-	function addAvatar(users){
-		users.forEach(function(user){
-				var newAvatar = Mustache.render($('#avatar-template').html(), user);
-				$(newAvatar).prependTo('#content');
-				$("#"+user.id).css("width", user.avatar_size);
-				$("#"+user.id).css("height", user.avatar_size);
-		});
-	};
+  addRepo("rails/rails");
+  
+  $(".search").on("submit", function(event) {
+    var repoEntered;
+    
+    event.preventDefault();
+    $("img").remove();
+    repoEntered = $("#repo").val();
+    addRepo(repoEntered);
+  });
 });
+
+function addAvatar(users){
+    users.forEach(function(user){
+      var newAvatar;
+
+      newAvatar = Mustache.render($('#avatar-template').html(), user);
+      
+      $(newAvatar).prependTo('#content');
+      $("#"+user.id).css("width", user.avatarSize);
+      $("#"+user.id).css("height", user.avatarSize);
+    
+    });
+  };
+
+function addRepo(repository) {
+  $.get("https://api.github.com/repos/" + repository + "/commits", function(commits) {
+    var firstTwentyCommits, imageSizer, users;
+    
+    firstTwentyCommits = commits.slice(0, 20);
+    imageSizer = new ImageSizer(firstTwentyCommits);
+    users = imageSizer.calculateSize();
+    
+    addAvatar(users);
+  });
+};
