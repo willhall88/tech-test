6373e36a888b4199d7133142da4462338a993683
willhall88 <willhall88@hotmail.com>
2014-08-20 12:58:57 +0100

    made some changes to the look of the graph

--- php-code-test/test.php
+++ php-code-test/test.php
@@ -1,6 +1,11 @@
 <?php
-  // ini_set('display_errors',1);  
+  // ini_set('display_errors',1);
   // error_reporting(E_ALL);
+  // Change this defines to where Your fonts are stored
+  DEFINE("TTF_DIR","/Library/Fonts/");
+   
+  // Change this define to a font file that You know that You have
+  DEFINE("TTF_FONTFILE","arial.ttf");
 
   require_once ('jpgraph/src/jpgraph.php');
   require_once ('jpgraph/src/jpgraph_scatter.php');
@@ -16,7 +21,7 @@
   $my_xpath_query = "//*[@id='mw-content-text']/table[1]//td";
   $table = $xpath->query($my_xpath_query);
   $year = [];
-  $revenue = [];
+  $revenues = [];
 
   foreach ($table as $film_info){
     $subject = $film_info->textContent;
@@ -27,24 +32,38 @@
       array_push($year, $subject);
     }
     elseif (preg_match($currency_pattern, $subject)) {
-      $money = str_replace(array('$', ','), '' , $subject);
-      array_push($revenue, $money);
+      $revenue = str_replace(array('$', ','), '' , $subject);
+      $revenue = ($revenue/1000000000);
+      array_push($revenues, $revenue);
     }
 
   }
 
   $graph = new Graph(900,600);
-  $graph->SetScale("linlin");
+  $graph->SetScale('linlin',0.4,3,1976,2015); 
+  $graph->yaxis->scale->ticks->Set(0.2);
 
-  $graph->img->SetMargin(100,100,100,100);    
-  $graph->SetShadow();
+  $graph->img->SetMargin(100,100,100,100);
+
+  $graph->ygrid->SetFill(true,'#EFEFEF','#BBCCFF'); 
+  $graph->xgrid->SetColor('#DDDDDD');
+  $graph->xgrid->Show();
 
   $graph->title->Set("Adjusted Gross Revenue vs Film Year");
-  $graph->title->SetFont(FF_FONT1,FS_BOLD);
+  $graph->xaxis->SetTitle("Year", "center");
+  $graph->yaxis->SetTitle('Gross Revenue - $Billion', "center");
+  $graph->xaxis->SetFont(FF_ARIAL, FS_NORMAL, 10);
+  $graph->yaxis->SetFont(FF_ARIAL, FS_NORMAL, 10);
+  $graph->title->SetFont(FF_ARIAL, FS_NORMAL, 18);
 
-  $sp1 = new ScatterPlot($revenue,$year);
+  $sp1 = new ScatterPlot($revenues,$year);
+
+  $sp1->mark->SetType(MARK_FILLEDCIRCLE);
+  $sp1->mark->SetWidth(4);
+  $sp1->mark->SetColor("blue");
 
   $graph->Add($sp1);
   $graph->Stroke();
 ?>
+    
     
